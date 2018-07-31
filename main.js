f8d5b362121a37ae74fe9d71e70e6d9f4b1f9810
RafaAudibert <rafaeelaudibert@gmail.com>
2018-07-31 11:39:08 -0300

    :boom: Update the tree exhibition to use treeView.js (used as submodule)

--- main.js
+++ main.js
@@ -1,90 +1,21 @@
-// Labels for the status classes
-const labels = {
-	'TL': 'learn',
-	'R': 'revisit',
-	'LR': 'learning',
-	'L': 'learned'
-}
-
-// Calls the function to insert the tree, in #treeId
-insertTreeLevel($('#treeId'), technologies);
-
-// Recursive function which inserts the data in the root
-function insertTreeLevel(root, list) {
-
-	//For each object in the list
-	for (obj of list) {
-
-		// Inserts the li
-		const li = $('<li>');
-		root.append(li);
-
-		// Inserts the li text, with it's class
-		li.append($('<span>')
-			.append(obj.name)
-			.addClass(labels[obj.status]));
-
-		// If the object has children
-		if (obj.data) {
-
-			// Adds '+' after the text, and the folder class
-			li.children()
-				.append(' +')
-				.addClass('folder');
-
-			// Creates the ul, appending it to the actual li
-			const ul = $('<ul>');
-			li.append(ul);
-
-			// Inserts the children in the tree
-			insertTreeLevel(ul, obj.data);
-		}
-	}
-
-	// Hiding all the folder children asap, so that they do not appear on reloading
-	document.querySelectorAll('.folder')
-		.forEach(folder => folder.nextElementSibling.style.display = 'none');
-}
-
-// Initializing stuff
-$(document)
-	.ready(() => {
-
-		// Select all the folder class elements
-		document.querySelectorAll('.folder')
-			.forEach(folder => {
-
-				// Adding toggle option when clicking in a folder
-				folder.onclick = (e) => {
-					const ul = folder.nextElementSibling
-					const display = ul.style.display
-
-					if (display === 'none') {
-						ul.style.display = 'block'
-					} else {
-						ul.style.display = 'none'
-					}
-				}
-			})
-
-	})
-
-// Setting expand-all button function
-document.getElementById('expand-all')
-	.addEventListener('click', () => {
-		document.querySelectorAll('.folder')
-			.forEach(folder => {
-				const ul = folder.nextElementSibling
-				ul.style.display = 'block'
-			})
-	})
-
-// Setting close-all button function
-document.getElementById('close-all')
-	.addEventListener('click', () => {
-		document.querySelectorAll('.folder')
-			.forEach(folder => {
-				const ul = folder.nextElementSibling
-				ul.style.display = 'none'
-			})
-	})
+// Creates the tree, instantiating all the buttons
+const tree = new treeView('treeId', technologies, {
+    labels: {
+      'TL': 'learn',
+      'R': 'revisit',
+      'LR': 'learning',
+      'L': 'learned'
+    },
+    dataClass: 'folder',
+    hide: 'true',
+    subTreeSignClosed: '+',
+    subTreeSignOpened: '-',
+    toggle: true,
+    warning: true,
+    firstOpen: true
+  })
+  .createTree()
+  .setAction('click', 'expand', 'expand-all')
+  .setAction('click', 'close', 'close-all')
+  .setAction('click', 'hide', 'hide-tree')
+  .setAction('click', 'show', 'show-tree');
